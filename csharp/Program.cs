4a9f8e397a83fb948f3259db2c2d80e2d180f7fa
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-02-08 11:32:02 -0800

    added c# trigger

---
+++ csharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace ailTrigger
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var webClient = new WebClient();
+			webClient.DownloadString("http://192.168.7.133/ail/ailApp/_packages/notifications.php");
+		}
+	}
+}
